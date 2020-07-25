dd1478da27d01ab5d1733721b08d65c476c34586
smessina <info@silviomessina.com>
2020-07-25 11:56:54 -0300

    Configuraciones de compilación para deploy en GitHub Pages

---
+++ docs/precache-manifest.e63624dcfa54c09c1ac55a477a51f580.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c6204040dedf6b80526e5cd97de686",
+    "url": "/BootstReact/index.html"
+  },
+  {
+    "revision": "aa861ac891f8c7e86ea6",
+    "url": "/BootstReact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7f8fb3e6513b0d3868af",
+    "url": "/BootstReact/static/css/main.281f06d7.chunk.css"
+  },
+  {
+    "revision": "aa861ac891f8c7e86ea6",
+    "url": "/BootstReact/static/js/2.72412679.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/BootstReact/static/js/2.72412679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8fb3e6513b0d3868af",
+    "url": "/BootstReact/static/js/main.90baeef6.chunk.js"
+  },
+  {
+    "revision": "2a4e6045760ef4500688",
+    "url": "/BootstReact/static/js/runtime-main.56528803.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BootstReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
