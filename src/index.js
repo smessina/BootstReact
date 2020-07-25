811cd4741b823bdaac0faa73d53e12173fc49083
smessina <info@silviomessina.com>
2020-07-25 09:03:45 -0300

    Incorporando dependencias de Bootstrap 4 a index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+window.jQuery = window.$ = $
+window.Popper = Popper
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
